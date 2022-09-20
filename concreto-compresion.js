09318820816ae092958960f7c21955fc2ac74d9c
bayron479 <bayron.ssmm@gmail.com>
2022-09-20 08:37:01 -0500

    20-09-8:30

--- concreto-compresion.js
+++ concreto-compresion.js
@@ -162,7 +162,7 @@ var tiempoFalla;
 //var finEnsayo = datosExcel.length*1000;
 
 function mensajeCargaMaxima() {
-	tiempoFalla = setTimeout("datoCargaMaxima()", 45000);
+	tiempoFalla = setTimeout("datoCargaMaxima()", 50000);
 }
 
 function datoCargaMaxima() {
@@ -184,25 +184,25 @@ function datosEnsayo() {
 	
 // Animación 3d-gltf
  
- var scene = new THREE.Scene();
- var camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
- var cilindroConcreto; var maquinaCarga;
- const renderer = new THREE.WebGLRenderer();
- renderer.setSize(window.innerWidth, window.innerHeight);
- const loader = new GLTFLoader();
- var obj;
- //loader.load('concreto-compresion.gltf', function (gltf) {
- loader.load('concreto-compresion.glb', function (gltf) {
- obj = gltf.scene;
- scene.add(gltf.scene);
-   }, undefined, function (error) {
- 	console.error( error );
- });	
- function animate() {
- 	requestAnimationFrame( animate );
- 	renderer.render( scene, camera );
- }
- animate();
+//  var scene = new THREE.Scene();
+//  var camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
+//  var cilindroConcreto; var maquinaCarga;
+//  const renderer = new THREE.WebGLRenderer();
+//  renderer.setSize(window.innerWidth, window.innerHeight);
+//  const loader = new GLTFLoader();
+//  var obj;
+//  //loader.load('concreto-compresion.gltf', function (gltf) {
+//  loader.load('concreto-compresion.glb', function (gltf) {
+//  obj = gltf.scene;
+//  scene.add(gltf.scene);
+//    }, undefined, function (error) {
+//  	console.error( error );
+//  });	
+//  function animate() {
+//  	requestAnimationFrame( animate );
+//  	renderer.render( scene, camera );
+//  }
+//  animate();
 
 	
 							
