47d6a243ddad5150117c1c4d430487b0a2252654
bayron479 <bayron.ssmm@gmail.com>
2022-09-20 11:27:07 -0500

    20-09-11:20

--- concreto-compresion.js
+++ concreto-compresion.js
@@ -184,11 +184,31 @@ function datosEnsayo() {
 	
 // Animación 3d-gltf
  
-//  var scene = new THREE.Scene();
-//  var camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
+
+// const loader = new GLTFLoader();
+
+// loader.load('concreto-compresion.gltf', function(gltf) {
+
+// 	scene.add(gltf.scene);
+
+// }, undefined, function (error) {
+
+// 	console.error(error);
+
+// });
+
+// function animate() {
+// 	requestAnimationFrame( animate );
+// 	renderer.render( scene, camera );
+// }
+// animate();
+
 //  var cilindroConcreto; var maquinaCarga;
-//  const renderer = new THREE.WebGLRenderer();
-//  renderer.setSize(window.innerWidth, window.innerHeight);
 //  const loader = new GLTFLoader();
 //  var obj;
 //  //loader.load('concreto-compresion.gltf', function (gltf) {
@@ -198,11 +218,7 @@ function datosEnsayo() {
 //    }, undefined, function (error) {
 //  	console.error( error );
 //  });	
-//  function animate() {
-//  	requestAnimationFrame( animate );
-//  	renderer.render( scene, camera );
-//  }
-//  animate();
+  
 
 	
 							
